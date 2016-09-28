5482e9752daec1cafe65d1b2e8677b6c6d474db4
Amber_Sethi <ambersethi97@gmail.com>
2016-09-28 18:16:59 +0530

    Add files via upload

---
+++ 2Darraymu.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+int a[2][2], b[2][2],c[2][2], i, j, k, sum=0;
+for(i=0 ; i<2 ; i++)
+{
+    for(j=0 ; j<2 ; j++){
+        scanf("%d" , &a[i][j]);
+    }
+}
+for(i=0 ; i<2 ; i++)
+{
+    for(j=0 ; j<2 ; j++){
+        scanf("%d", &b[i][j]);
+    }   
+}
+for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+        for (k = 0; k < 2; k++) {
+          sum = sum + a[i][k]*b[k][j];
+        }
+ 
+        c[i][j] = sum;
+        sum = 0;
+      }
+    }
+ 
+    printf("Product of entered matrices:-\n");
+ 
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++){
+        printf("%d\t", c[i][j]);
+ 
+      printf("\n");
+    }
+  }
+return 0;
+}
